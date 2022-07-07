ddbe8cc32c8c61ebc83229d7eabac9d535b0546d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-06 22:42:58 -0700

    created a limit for the long by declaring the largest long prime number

--- src/PowerOfNumbersBinarySearch.java
+++ src/PowerOfNumbersBinarySearch.java
@@ -1,14 +1,14 @@
 public class PowerOfNumbersBinarySearch {
-
+    static long mod = (long)1e9*7;
     public static long powerOfNumbersBinarySearch(int n, int a){
         if(n==1){
             return a;
         }
         long storage = powerOfNumbersBinarySearch(n/2, a);
         if(n%2 ==1){
-            return storage*storage* a;
+            return ((storage*storage)%mod* a)%mod;
         }else{
-            return storage*storage;
+            return (storage*storage)%mod;
         }
 
     }
